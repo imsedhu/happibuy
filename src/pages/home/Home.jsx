a83b520950802ca72057b4bd352a8e9157219720
imsedhu <sedhuramansedhu17@gmail.com>
2024-10-17 19:36:54 +0530

    home category component added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Banner from './Banner'
+import HomeCategory from './HomeCategory'
 
 const Home = () => {
   return (
+    <>
     <Banner />
+    <HomeCategory />
+    </>
   )
 }
 
